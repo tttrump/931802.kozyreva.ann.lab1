b3df01bffe80ea21f8b9e04e33892cd3124e98fd
Kozyreva Ann <anyta_winterr@mail.ru>
2021-06-08 15:58:00 +0700

    Add new files

---
+++ web1/Lab1/Controllers/CalcServiceController.cs
@@ -0,0 +1,55 @@
+﻿using Lab1.Models;
+using Lab1.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class CalcServiceController : Controller
+    {
+        private readonly CalcService _service;
+
+        public CalcServiceController(CalcService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PassUsingModel()
+        {
+            return View(_service.GetCalcModel());
+        }
+
+        public IActionResult PassUsingViewData()
+        {
+            var model = _service.GetCalcModel();
+            ViewData["X"] = model.X;
+            ViewData["Y"] = model.Y;
+            ViewData["Sum"] = model.Sum;
+            ViewData["Dif"] = model.Dif;
+            ViewData["Mult"] = model.Mult;
+            ViewData["Div"] = model.Div;
+            return View();
+        }
+
+        public IActionResult PassUsingViewBag()
+        {
+            var model = _service.GetCalcModel();
+            ViewBag.X = model.X;
+            ViewBag.Y = model.Y;
+            ViewBag.Sum = model.Sum;
+            ViewBag.Dif = model.Dif;
+            ViewBag.Mult = model.Mult;
+            ViewBag.Div = model.Div;
+            return View();
+        }
+
+        public IActionResult AccessServiceDirectly()
+        {
+            return View();
+        }
+    }
+}
---
+++ web1/Lab1/Startup.cs
@@ -0,0 +1,27 @@
+using Lab1.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lab1 {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
+            services.AddScoped<CalcService>();
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute("default", "{controller=CalcService}/{action=Index}");
+            });
+        }
+    }
+}
